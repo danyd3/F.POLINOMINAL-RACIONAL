911b5180ca830a45d94e468e8159f835c0ccb752
danyd3 <89928594+danyd3@users.noreply.github.com>
2021-11-19 18:52:18 -0600

    Add files via upload

---
+++ Función_5.m
@@ -0,0 +1,26 @@
+% Title             :Funciones algrbraicas: Polinomiales y racionales #5
+% Description       :Script para de funciones #1
+% Author            :Daniel Alberto De Jesús (danyd3)
+% Date              :20211116
+% Version           :1
+% Usage             :C:\Users\Documents\Octave
+% Note              :Requiere octave y la paqueteria symbolic
+
+%Limpiar variables
+clear
+%Cargar symbolic
+pkg load symbolic
+syms x
+%Dominio
+x = -10:1:10;
+%Funcion
+fx= 2 * x + 1;
+%Funcion plotear
+plot (x, fx);
+
+hold on
+grid on
+
+%Datos de grafica
+title(["Funcion irracional"]);
+disp ("Es una funcion irracional");
